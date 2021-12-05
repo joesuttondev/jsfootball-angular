a405b42c9fd0d24d39958e74ac2c9adc97a9cea6
joesuttondev <joesuttondevelopment@gmail.com>
2021-12-05 23:52:06 +0000

    Sort results date ascending

--- src/app/fixture.service.ts
+++ src/app/fixture.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Fixture, Score, Period } from './fixture';
-import { Observable, of } from 'rxjs';
+import { Observable, of, tap, map } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
@@ -15,7 +15,11 @@ export class FixtureService {
     return this.http.get<Fixture[]>(this.fixturesURL + '?status=SCHEDULED');
   }
   getResults(): Observable<Fixture[]> {
-    return this.http.get<Fixture[]>(this.fixturesURL + '?status=FINISHED');
+    return this.http.get<Fixture[]>(this.fixturesURL + '?status=FINISHED').pipe(
+      map(fixtures => fixtures.sort((a: Fixture, b: Fixture) =>
+        (new Date(b.utcDate)).getTime() - (new Date(a.utcDate)).getTime()
+      ))
+    );
   }
 
   // getFixtures(): Observable<DayC[]> {
